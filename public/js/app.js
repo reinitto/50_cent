482d0b2bb6bdaa9e5e5ec102fd7f381c4f4931b4
Labais <reinitto@gmail.com>
2018-12-20 18:43:47 +0100

    added nightMode, surge price multiplier

--- public/js/app.js
+++ public/js/app.js
@@ -1,132 +1,52 @@
-const from = document.getElementById("from");
+const fromInput = document.getElementById("from");
 const uber = document.getElementById("uber");
 const fifty = document.getElementById("fifty");
 const info = document.getElementsByClassName("info");
+const surge = document.getElementById("surge");
+let surge_text = document.getElementById("surge_text");
 const toggle_from = document.getElementById("toggle_from");
-const to = document.getElementById("to");
+const toInput = document.getElementById("to");
 const inputs = document.getElementById("inputs");
 const from_container = document.getElementById("from_container");
 const distance = document.getElementById("distance");
 const time = document.getElementById("time");
 const submit = document.getElementById("getRoute");
-let map, currentPos;
-
-console.log("info: ", info);
-
-//Toggles FROM input
-toggle_from.addEventListener("click", function() {
-  if (from_container.style.visibility === "hidden") {
-    from.value = "";
-    from_container.style.visibility = "visible";
-    toggle_from.textContent = "User current position";
-  } else {
-    from_container.style.visibility = "hidden";
-    toggle_from.textContent = "Different starting point?";
-    getLocation.then(location => {
-      initMap(location, to.value);
-    });
+//Uber pricing constraints
+const INITIAL_COST = 0.4;
+const PRICE_PER_KM = 0.6;
+const PRICE_PER_MIN = 0.14;
+const SERVICE_COST = 1.58;
+const MIN_PRICE = 10;
+let currentPos;
+//fake surge multiplier
+const getSurgeRatio = (
+  hour = new Date().getHours(),
+  day = new Date().getDay()
+) => {
+  if (day == 4 || 6) {
+    if (hour > 17 && hour < 25) {
+      return 1 + (hour - 17) * 0.5;
+    }
+    if (hour < 6) {
+      return 1 + hour / 2;
+    }
   }
-});
-// onSubmit click handler
-submit.addEventListener("click", function(e) {
-  e.preventDefault();
-  if (!from.value) {
-    console.log("no from value!");
-    getLocation.then(location => {
-      console.log(location);
-      if (location) initMap(location, to.value);
-    });
-  } else {
-    initMap(from.value, to.value);
-    console.log(from.value);
-    console.log(to.value);
-  }
+  return 1;
+};
 
-  console.log("Submit");
-});
+const SURGE_MULTIPLIER = getSurgeRatio();
 
-function initMap(a, b) {
-  //Hide info class fields
-
-  //get current location if FROM location is not provided
-  if (navigator.geolocation && !a) {
-    getLocation.then(location => {
-      //Set a marker at current location
-      new google.maps.Marker({
-        position: location,
-        map: map,
-        title: "You're here"
-      });
-      //center on current/initial location
-      map.setCenter(location);
-      //set current location variable
-      a = location;
-    });
-  }
-  //From location
-  var pointA = a,
-    //To location
-    pointB = b,
-    // Map options
-    myOptions = {
-      zoom: 13,
-      center: pointB ? null : pointA,
-      gestureHandling: "greedy",
-      disableDefaultUI: true,
-      scrollwheel: false
-    },
-    map = new google.maps.Map(document.getElementById("map"), myOptions),
-    // Instantiate a directions service.
-    directionsService = new google.maps.DirectionsService(),
-    directionsDisplay = new google.maps.DirectionsRenderer({
-      map: map
-    });
-  // Google places API
-  // Adds autocomplete to FROM input field
-  var autocompleteFrom = new google.maps.places.Autocomplete(from);
-  google.maps.event.addListener(autocompleteFrom, "place_changed", function() {
-    var place = autocompleteFrom.getPlace();
-    console.log("From: ", place);
-  });
-
-  // Google places API
-  // Adds autocomplete to TO input field
-  var autocompleteTo = new google.maps.places.Autocomplete(to);
-  google.maps.event.addListener(autocompleteTo, "place_changed", function() {
-    var place = autocompleteTo.getPlace();
-    console.log("TO: ", place);
-  });
-  // Calculate distance and Route
-  // only when destination is entered
-  if (pointB) {
-    //get Distance from A to b
-    calculateDistance(pointA, pointB);
-
-    // get route from A to B
-    calculateAndDisplayRoute(
-      directionsService,
-      directionsDisplay,
-      pointA,
-      pointB
-    );
-  }
-}
 // Calculates and displays route
 function calculateAndDisplayRoute(
   directionsService,
   directionsDisplay,
-  pointA,
-  pointB
+  fromLoc,
+  toLoc
 ) {
-  //Hide info class fields
-  [...info].forEach(tag => {
-    tag.style.visibility = "visible";
-  });
-
   directionsService.route(
     {
-      origin: pointA,
-      destination: pointB,
+      origin: fromLoc,
+      destination: toLoc,
       avoidTolls: true,
       avoidHighways: false,
       travelMode: google.maps.TravelMode.DRIVING
@@ -135,17 +55,20 @@ function calculateAndDisplayRoute(
       if (status == google.maps.DirectionsStatus.OK) {
         directionsDisplay.setDirections(response);
       } else {
+        clearInputs(fromInput, toInput);
         window.alert("Directions request failed due to " + status);
       }
     }
   );
+  //Makes Price, distance and time visible
+  showInfo();
 }
 
-//Calculate Distance from A to b
+//Calculate Distance from A to B
 const calculateDistance = (from, to) => {
-  var service = new google.maps.DistanceMatrixService();
-  console.log("from: ", from);
-  service.getDistanceMatrix(
+  //Instantiate distance service
+  const distanceService = new google.maps.DistanceMatrixService();
+  distanceService.getDistanceMatrix(
     {
       origins: [from],
       destinations: [to],
@@ -153,28 +76,43 @@ const calculateDistance = (from, to) => {
     },
     calculateDistanceCallback
   );
-  // calculateDistance callback
-  function calculateDistanceCallback(response, status) {
-    if (status == "OK") {
-      const initialCost = 0.4;
-      const pricePerKm = 0.6;
-      const pricePerMin = 0.14;
-      const serviceCost = 1.58;
-      console.log("distance: ", response);
-      var results = response.rows[0].elements;
-      distance.textContent = results[0].distance.text;
-      time.textContent = results[0].duration.text;
-      var uberPrice =
-        initialCost +
-        serviceCost +
-        (pricePerKm * results[0].distance.value) / 1000 +
-        (pricePerMin * results[0].duration.value) / 60;
-      uber.textContent = uberPrice.toFixed(1) + " euros";
-      fifty.textContent = uberPrice.toFixed(2) - 0.5 + " euros";
-    }
+};
+
+// calculateDistance callback
+function calculateDistanceCallback(response, status) {
+  if (status == "OK") {
+    let results = response.rows[0].elements;
+    distance.textContent = results[0].distance.text;
+    time.textContent = results[0].duration.text;
+    //Makes the surge text visible
+    isSurge();
+    let uberPrice =
+      INITIAL_COST +
+      SERVICE_COST +
+      ((PRICE_PER_KM * results[0].distance.value) / 1000 +
+        (PRICE_PER_MIN * results[0].duration.value) / 60) *
+        SURGE_MULTIPLIER;
+    uber.textContent =
+      (uberPrice < MIN_PRICE ? MIN_PRICE.toFixed(1) : uberPrice.toFixed(1)) +
+      " euros";
+    fifty.textContent =
+      (uberPrice < MIN_PRICE
+        ? MIN_PRICE.toFixed(2) - 0.5
+        : uberPrice.toFixed(2) - 0.5) + " euros";
+  }
+}
+const isSurge = () => {
+  if (SURGE_MULTIPLIER > 1) {
+    //set text
+    surge_text.textContent = `SURGE: ${SURGE_MULTIPLIER}!!`;
+    //set visibility
+    surge.style.visibility = "visible";
+  } else {
+    surge.style.visibility = "hidden";
   }
 };
-//returns your location Promise
+
+//Get location Promise
 const getLocation = new Promise((resolve, reject) => {
   navigator.geolocation.getCurrentPosition(function(position) {
     if (position) {
@@ -187,3 +125,200 @@ const getLocation = new Promise((resolve, reject) => {
     }
   });
 });
+
+//Show Info (Distance and Time) fields
+const showInfo = function() {
+  [...info].forEach(tag => {
+    tag.style.visibility = "visible";
+  });
+};
+
+//Toggles FROM input
+const toggleFromClickHandler = () => {
+  if (
+    from_container.style.visibility === "hidden" ||
+    from_container.style.visibility === ""
+  ) {
+    fromInput.value = "";
+    from_container.style.visibility = "visible";
+    toggle_from.textContent = "User current position";
+  } else {
+    from_container.style.visibility = "hidden";
+    toggle_from.textContent = "Different starting point?";
+
+    getLocation.then(location => {
+      if (!toInput.value) {
+        initMap();
+      } else {
+        initMap(location, toInput.value);
+      }
+    });
+  }
+};
+// onSubmit click handler
+const submitClickHandler = e => {
+  e.preventDefault();
+  if (!fromInput.value) {
+    getLocation.then(location => {
+      if (location) initMap(location, toInput.value);
+    });
+  } else {
+    initMap(fromInput.value, toInput.value);
+  }
+};
+submit.addEventListener("click", submitClickHandler);
+toggle_from.addEventListener("click", toggleFromClickHandler);
+//Clears values of given arguments
+const clearInputs = (...inputs) => {
+  inputs.forEach(input => {
+    input.value = "";
+  });
+};
+function initMap(a, b) {
+  let fromLoc = a,
+    toLoc = b,
+    map;
+  // Map options
+  const MAP_OPTIONS = {
+    zoom: 13,
+    center: toLoc ? null : fromLoc,
+    gestureHandling: "greedy",
+    disableDefaultUI: true,
+    scrollwheel: false,
+    styles: new Date().getHours() > 17 ? nightStyles : null
+  };
+  //get current location if FROM location is not provided
+  if (navigator.geolocation && !a) {
+    getLocation.then(location => {
+      //Set a marker at current location
+      new google.maps.Marker({
+        position: location,
+        map: map,
+        title: "You're here",
+        animation: google.maps.Animation.BOUNCE
+      });
+      //center on current/initial location
+      map.setCenter(location);
+      //set current location variable
+      a = location;
+    });
+  }
+  //Instantiate google maps
+  map = new google.maps.Map(document.getElementById("map"), MAP_OPTIONS);
+  // Instantiate a directions service.
+  directionsService = new google.maps.DirectionsService();
+  directionsDisplay = new google.maps.DirectionsRenderer({
+    map: map
+  });
+
+  // Google places API
+  // Adds autocomplete to FROM input field
+  var autocompleteFrom = new google.maps.places.Autocomplete(fromInput);
+  // Adds autocomplete to TO input field
+  var autocompleteTo = new google.maps.places.Autocomplete(toInput);
+
+  // Calculate distance and Route
+  // only when destination is entered
+  if (toLoc) {
+    //get Distance from A to b
+    calculateDistance(fromLoc, toLoc);
+
+    // calc and display route from A to B
+    calculateAndDisplayRoute(
+      directionsService,
+      directionsDisplay,
+      fromLoc,
+      toLoc
+    );
+  }
+
+  //FROM, TO input event listeners
+  google.maps.event.addListener(autocompleteFrom, "place_changed", function() {
+    autocompleteFrom.getPlace();
+  });
+  google.maps.event.addListener(autocompleteTo, "place_changed", function() {
+    autocompleteTo.getPlace();
+  });
+}
+
+//I need to get rid of this thing...
+const nightStyles = [
+  { elementType: "geometry", stylers: [{ color: "#242f3e" }] },
+  { elementType: "labels.text.stroke", stylers: [{ color: "#242f3e" }] },
+  { elementType: "labels.text.fill", stylers: [{ color: "#746855" }] },
+  {
+    featureType: "administrative.locality",
+    elementType: "labels.text.fill",
+    stylers: [{ color: "#d59563" }]
+  },
+  {
+    featureType: "poi",
+    elementType: "labels.text.fill",
+    stylers: [{ color: "#d59563" }]
+  },
+  {
+    featureType: "poi.park",
+    elementType: "geometry",
+    stylers: [{ color: "#263c3f" }]
+  },
+  {
+    featureType: "poi.park",
+    elementType: "labels.text.fill",
+    stylers: [{ color: "#6b9a76" }]
+  },
+  {
+    featureType: "road",
+    elementType: "geometry",
+    stylers: [{ color: "#38414e" }]
+  },
+  {
+    featureType: "road",
+    elementType: "geometry.stroke",
+    stylers: [{ color: "#212a37" }]
+  },
+  {
+    featureType: "road",
+    elementType: "labels.text.fill",
+    stylers: [{ color: "#9ca5b3" }]
+  },
+  {
+    featureType: "road.highway",
+    elementType: "geometry",
+    stylers: [{ color: "#746855" }]
+  },
+  {
+    featureType: "road.highway",
+    elementType: "geometry.stroke",
+    stylers: [{ color: "#1f2835" }]
+  },
+  {
+    featureType: "road.highway",
+    elementType: "labels.text.fill",
+    stylers: [{ color: "#f3d19c" }]
+  },
+  {
+    featureType: "transit",
+    elementType: "geometry",
+    stylers: [{ color: "#2f3948" }]
+  },
+  {
+    featureType: "transit.station",
+    elementType: "labels.text.fill",
+    stylers: [{ color: "#d59563" }]
+  },
+  {
+    featureType: "water",
+    elementType: "geometry",
+    stylers: [{ color: "#17263c" }]
+  },
+  {
+    featureType: "water",
+    elementType: "labels.text.fill",
+    stylers: [{ color: "#515c6d" }]
+  },
+  {
+    featureType: "water",
+    elementType: "labels.text.stroke",
+    stylers: [{ color: "#17263c" }]
+  }
+];
--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 var express = require("express");
-var cors = require("cors");
 var app = express();
 var port = 3000 || process.env.PORT;
 
-app.use(cors());
 app.use(express.static(__dirname + "/public"));
 
 app.listen(port, () => {
